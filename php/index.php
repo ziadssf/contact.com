610a2da8527182750ab09cfef4fe42005d57e4a4
ziadssf <anassssf.ziadm@gmail.com>
2021-05-19 23:48:54 +0200

    contact us form

---
+++ php/index.php
@@ -0,0 +1,13 @@
+<?php
+
+   $name = $_POST['name'];
+   $user_email = $_POST['email'];
+   $message = $_POST['msg'];
+   $phone = $_POST['phone'];
+   $my_email = 'alpha4code@gmail.com';
+   $subject = "new message from $name .";
+   $headers = "From: $user_email \r\n";
+   $email_body = "User Name: $name. \n" . "User Email: $user_email. \n" . "User Message: $message. \n" . "User Phone: $phone. \n";
+   mail($my_email, $subject,$email_body,$headers);
+
+?>
