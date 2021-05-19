caf62726054d877e6f730ffa781d836d0da02f1b
Ziad Mohamed <79987382+ziadssf@users.noreply.github.com>
2021-05-20 00:00:34 +0200

    Update index.php

--- php/index.php
+++ php/index.php
@@ -5,7 +5,7 @@
    $message = $_POST['msg'];
    $phone = $_POST['phone'];
    $my_email = 'alpha4code@gmail.com';
-   $subject = "new message from $name .";
+   $subject = "new message";
    $headers = "From: $user_email \r\n";
    $email_body = "User Name: $name. \n" . "User Email: $user_email. \n" . "User Message: $message. \n" . "User Phone: $phone. \n";
    mail($my_email, $subject,$email_body,$headers);
